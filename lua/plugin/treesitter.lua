d833c900b2740e7f464e3cceffbcb7a0451024fa
Manish Singh <30594311+singh-man@users.noreply.github.com>
2026-08-21 12:08:40 +0100

    migrate Tree-sitter configuration to Neovim 0.12 (#35)

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,10 +1,66 @@
-require'nvim-treesitter'.setup {
-    ensure_installed = {"bash", "c", "clojure", "cmake", "cpp", "css", "dockerfile", "go", "html", "java", "javascript", "json", "kotlin", "latex", "lua", "php", "python", "rust", "toml", "typescript", "vim", "yaml"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    auto_install = true,
-    ignore_install = { "dart" }, -- List of parsers to ignore installing
-    incremental_selection = { enable = true }
+local treesitter = require("nvim-treesitter")
+
+local parsers = {
+  "bash",
+  "c",
+  "clojure",
+  "cmake",
+  "cpp",
+  "css",
+  "dockerfile",
+  "go",
+  "html",
+  "java",
+  "javascript",
+  "json",
+  "kotlin",
+  "latex",
+  "lua",
+  "php",
+  "python",
+  "rust",
+  "toml",
+  "typescript",
+  "vim",
+  "yaml",
 }
 
--- Code folding
+treesitter.setup({
+  install_dir = vim.fn.stdpath("data") .. "/site",
+})
+
+local configured_parsers = {}
+for _, parser in ipairs(parsers) do
+  configured_parsers[parser] = true
+end
+
+local function start_highlighting(bufnr)
+  if not vim.api.nvim_buf_is_valid(bufnr) then
+    return
+  end
+
+  local language = vim.treesitter.language.get_lang(vim.bo[bufnr].filetype)
+  if language and configured_parsers[language] then
+    pcall(vim.treesitter.start, bufnr, language)
+  end
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    start_highlighting(args.buf)
+  end,
+})
+
+-- Installation is asynchronous and is a no-op for parsers already present.
+-- Start highlighting in any buffers opened while missing parsers were installed.
+treesitter.install(parsers):await(function()
+  vim.schedule(function()
+    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+      start_highlighting(bufnr)
+    end
+  end)
+end)
+
+-- Optional Tree-sitter folding:
 -- vim.opt.foldmethod = "expr"
--- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
