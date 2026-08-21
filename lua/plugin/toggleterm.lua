5d405057c5e7cd0c872bce8c22e772e437e717c2
Manish Singh <30594311+singh-man@users.noreply.github.com>
2026-08-21 14:04:27 +0100

    remove comment plugin and enable spell checking

--- lua/plugin/toggleterm.lua
+++ lua/plugin/toggleterm.lua
@@ -3,7 +3,7 @@ require"toggleterm".setup {
   open_mapping = [[<c-t>]],
   shade_filetypes = {},
   shade_terminals = true,
-  shading_factor = '1',
+  shading_factor = 1,
   start_in_insert = true,
   persist_size = true,
   direction = 'horizontal'
