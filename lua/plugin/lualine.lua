9023dcadeb6d7e1cb139bdd11c89a68041666695
Manish Singh <30594311+singh-man@users.noreply.github.com>
2026-08-22 15:45:41 +0100

    replace Barbar with native buffer navigation (#45)

--- lua/plugin/lualine.lua
+++ lua/plugin/lualine.lua
@@ -1,1 +1,30 @@
-require'lualine'.setup{}
+require("lualine").setup({
+  options = {
+    always_show_tabline = true,
+  },
+  tabline = {
+    lualine_a = {
+      {
+        "buffers",
+        mode = 0,
+        show_filename_only = true,
+        show_modified_status = true,
+        buffers_color = {
+          active = { gui = "bold" },
+          inactive = "lualine_c_inactive",
+        },
+        fmt = function(name, buffer)
+          return buffer.current and ("▌ " .. name) or name
+        end,
+        max_length = function()
+          return vim.o.columns
+        end,
+        symbols = {
+          modified = " ●",
+          alternate_file = "#",
+          directory = "",
+        },
+      },
+    },
+  },
+})
