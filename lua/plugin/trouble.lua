06056912d0e7b9fa9739423f13ae34d22e7b173c
Manish Singh <30594311+singh-man@users.noreply.github.com>
2026-08-24 11:22:57 +0100

    add Trouble navigation mappings (#47)

--- lua/plugin/trouble.lua
+++ lua/plugin/trouble.lua
@@ -3,3 +3,11 @@ require'trouble'.setup {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
 }
+
+vim.keymap.set("n", "<leader>lb", "<Cmd>Trouble diagnostics toggle filter.buf=0 focus=true<CR>", {
+    desc = "Buffer diagnostics (Trouble)",
+})
+
+vim.keymap.set("n", "<leader>lq", "<Cmd>Trouble qflist toggle focus=true<CR>", {
+    desc = "Quickfix list (Trouble)",
+})
